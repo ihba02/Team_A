366a45fc7c2401e2272c8fbabe34c11ee00e9798
ihba02 <ihba02@gmail.com>
2025-10-30 15:22:44 +0530

    table_creation

--- models/create_tables.py
+++ models/create_tables.py
@@ -1,31 +1,32 @@
 from sqlalchemy import create_engine, text
 
-# Create SQLite connection (use .db file extension)
-engine = create_engine("sqlite:///warehouse")
+
+def Table_creation():
+    engine = create_engine("sqlite:///warehouse")
 
 # Each statement separately
-create_table_1 = """
-CREATE TABLE IF NOT EXISTS core_dim_asset (
+    create_table_1 = """
+    CREATE TABLE IF NOT EXISTS core_dim_asset (
     ASSET_KEY INTEGER PRIMARY KEY AUTOINCREMENT,
     ASSET TEXT,
     CATEGORY TEXT,
     MANUFACTURER TEXT,
     INSTALLDATE DATETIME
-);
-"""
+    );
+    """
 
-create_table_2 = """
-CREATE TABLE IF NOT EXISTS core_dim_plant (
+    create_table_2 = """
+    CREATE TABLE IF NOT EXISTS core_dim_plant (
     PLANT_KEY INTEGER PRIMARY KEY AUTOINCREMENT,
     PLANT_NAME TEXT,
     REGION TEXT,
     MANAGER TEXT,
     ESTABLISHED_YEAR INTEGER
-);
-"""
+    );
+    """
 
-create_table_3 = """
-CREATE TABLE IF NOT EXISTS core_fact_asset_performance (
+    create_table_3 = """
+    CREATE TABLE IF NOT EXISTS core_fact_asset_performance (
     FACT_KEY INTEGER PRIMARY KEY AUTOINCREMENT,
     DATE DATETIME,
     ASSET TEXT,
@@ -36,11 +37,12 @@ CREATE TABLE IF NOT EXISTS core_fact_asset_performance (
     HEALTH_SCORE FLOAT,
     FOREIGN KEY (PLANT) REFERENCES core_dim_plant(PLANT_NAME),
     FOREIGN KEY (ASSET) REFERENCES core_dim_asset(ASSET)
-);
-"""
+    );
+    """
 
 # Execute each separately
-with engine.connect() as conn:
-    for ddl in [create_table_1, create_table_2, create_table_3]:
-        conn.execute(text(ddl))
-    conn.commit()
+    with engine.connect() as conn:
+        for ddl in [create_table_1, create_table_2, create_table_3]:
+            conn.execute(text(ddl))
+        conn.commit()
+
